1a661d6c6f25d90a9f7cd6a3f8bb00986104fbb1
Lucas Nithael <94084548+LucasNithael@users.noreply.github.com>
2022-07-29 01:48:13 +0000

    new

--- Main.cs
+++ Main.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Linq;
 
 
 //     [ OpCode  rs rt rd shamt functio ]
@@ -10,7 +11,10 @@ using System.Collections.Generic;
 public class Program {
   public static void Main(string[] args) {
     AbrirTxt("teste.txt");
+    Console.WriteLine(BinarioHexadecimal("10100110"));
+    //Console.WriteLine(BinarioHexadecimal2("1111"));
   }
+
   
   public static string Codificar(string s){
     string[] aux = s.Split(" ");
@@ -74,15 +78,15 @@ public class Program {
   public static string Formar(string a){
     string rt = " ";
     if(a.IndexOf("$")==-1){
-      rt = Conversor(a);
+      rt = DecimalBinario(a);
       return rt;
     }
     a = a.Remove(0, 1);
-    rt = Conversor(a);
+    rt = DecimalBinario(a);
     return rt;
   }
   
-  public static string Conversor(string x){
+  public static string DecimalBinario(string x){
     Stack<int> p = new Stack<int>();
     int num = int.Parse(x);
     int n;
@@ -105,11 +109,46 @@ public class Program {
       s = f.ReadLine();
     }
     f.Close(); 
-    foreach(object i in codes)
-      Console.WriteLine(i);
+   // foreach(object i in codes)
+    //  Console.WriteLine(i);
     return codes;
   }
-  
+  public static string BinarioHexadecimal(string s){
+    string aux = "";
+    string hex  = "";
+    for(int j=0; j<8; j++){
+      for(int i=0; i<4; i++){
+        aux = aux + s[i];
+        
+      }
+      hex = hex + BinarioHexadecimal2(aux);
+      s = s.Remove(0, 4);
+      
+      }
+    return hex;
+  }
+  public static string BinarioHexadecimal2(string numeroBinario){
+    int expoente = 0;
+    int numero;
+    int soma = 0;
+    string numeroInvertido = ReverteString(numeroBinario);
+    for (int i = 0; i < numeroInvertido.Length; i++){
+      numero = Convert.ToInt32(numeroInvertido.Substring(i, 1));
+      soma += numero * (int)Math.Pow(2, expoente);
+      expoente++;
+            }
+    string result = Convert.ToString(soma);
+    if(result=="10") result = "a";
+    if(result=="11") result = "b";
+    if(result=="12") result = "c";
+    if(result=="13") result = "d";
+    if(result=="14") result = "e";
+    if(result=="15") result = "f";
+    return result;
+  }
+  public static string ReverteString(string str){
+    return new string(str.Reverse().ToArray());
+  } 
 }
 
 
